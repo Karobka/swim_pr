6b4770e0bd073677e9e0c6c1783cf071e218fb96
Tim Wright <mithgirw@gmail.com>
2016-08-15 11:17:04 -0600

    adding to temp works

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ var MOCK_STATS = {
             itemId: 11111,
             swimrId: "1j2k3l4",
             swimrName: "Billy",
-            itemDate: "01/20/2015",
+            itemDate: "2015-02-15",
             itemType: "swim meet",
             itemName: "Winter Swim Meet",
             itemStroke: "backstroke",
@@ -17,7 +17,7 @@ var MOCK_STATS = {
             itemId: 22222,
             swimrId: "1j2k3l4",
             swimrName: "Billy",
-            itemDate: "01/24/2015",
+            itemDate: "2015-10-22",
             itemType: "practice",
             itemName: "Backstroke practice",
             itemStroke: "backstroke",
@@ -29,7 +29,7 @@ var MOCK_STATS = {
             itemId: 33333,
             swimrId: "2aej3k4l5",
             swimrName: "Sue",
-            itemDate: "04/10/2015",
+            itemDate: "2015-04-15",
             itemType: "swim_meet",
             itemName: "Regional Swim Meet",
             itemStroke: "butterfly",
@@ -41,7 +41,7 @@ var MOCK_STATS = {
             itemId: 44444,
             swimrId: "3z4x5c6v",
             swimrName: "Krazy Karl",
-            itemDate: "04/10/2015",
+            itemDate: "2015-10-22",
             itemType: "practice",
             itemName: "Butterfly practice",
             itemStroke: "butterfly",
@@ -52,14 +52,25 @@ var MOCK_STATS = {
     ]
 };
 
+
 function getItems(callMeBack) {
     setTimeout(function(){ callMeBack(MOCK_STATS)}, 200);
 }
+//function that generates unique id nums
+function make_id() {
+    var item_id = 1;
+    return function(){
+        return item_id++;
+    }
+}
+var update_id = make_id();
 
+//function that displays objects from the array
 function displayItem(item) {
     for (index in item.swim_history){
+        
         $('.results_list').append(
-                '<li>' + item.swim_history[index].swimrName + ' ' +
+                '<li class="event_record" value=' + update_id() + '>' + item.swim_history[index].swimrName + ' ' +
                     item.swim_history[index].itemDate + ' ' +
                     item.swim_history[index].itemName + ' ' +
                     item.swim_history[index].itemDistance + ' meter ' +
@@ -74,11 +85,37 @@ function getdisplayItems() {
     getItems(displayItem);
 }
 
-$(function() {
-    getdisplayItems();
-})
 
-$('.add_record').submit(function(event) {
-    event.preventDefault();
-    
+/** loop through the array
+for (var i = 0; i != clickidthingy; i++){
+    swim_history.splic(clickidthingy, 1);
+    break;
+}
+ */
+
+$(document).ready(function(){
+    $(function() {getdisplayItems();});
+    $(".add_record").submit(function(event){
+        event.preventDefault();
+        $(".results_list").children().remove();
+        MOCK_STATS.swim_history.push(
+            { itemId: update_id(), 
+            swimrName: $(".swimr_name").val(),
+            itemDate: $(".item_date").val(),
+            itemType: null,
+            itemName: $(".item_name").val(),
+            itemStroke: $(".item_stroke").val(),
+            itemDistance: $(".item_distance").val(),
+            finishTime: null,
+            ranking: null
+            }
+        );
+        getdisplayItems();
+        console.log(MOCK_STATS);
+    });
+    $(".event_record").on("click", function(){
+            var index = MOCK_STATS.swim_history.indexOf($(this).text());
+            alert(index);
+    });
 });
+
