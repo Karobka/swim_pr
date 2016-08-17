4e6107d7f2988be37a7e52e2fa03c3cf700d88b5
Tim Wright <mithgirw@gmail.com>
2016-08-16 22:21:11 -0600

    trying to fix static file service

--- server.js
+++ server.js
@@ -3,16 +3,14 @@ var app = express();
 var bodyParser = require('body-parser');
 var records = require('./records_db');
 
-//serve the HTML/CSS/JS files found in 'public'
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
+
+//serve the HTML/CSS/JS files found in 'public'
+app.use(express.static('public'));
+
 
 //app.listen(process.env.PORT || 3006);
 
